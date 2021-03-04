09b8c6eca7275690e3e764c3215371226bc5c837
yuki-snow1823 <magic.iroha@gmail.com>
2021-03-05 07:11:52 +0900

    add: 入力フォームの追加

--- src/components/Memo.vue
+++ src/components/Memo.vue
@@ -1,12 +1,20 @@
 <template>
   <h1>{{ pageTitle}}</h1>
-  
+  <!-- formサイズが変わらない -->
+  <div>
+    <input type="text" v-model="title" :width="formSize" placeholder="タイトルを入力してください。">
+  </div>
+  <div>
+    <input type="text" v-model="text" :width="formSize" placeholder="本文を入力してください。">
+  </div>
+
 </template>
 
 <script>
   export default {
     title: "memoのタイトル",
     text: "memoの本文",
+    formSize: "300",
     props: {
       pageTitle: String
     }
