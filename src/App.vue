b4f7df4bd9b875ed5a767f61ebea61baa1208376
yuki-snow1823 <magic.iroha@gmail.com>
2021-03-05 07:04:23 +0900

    add: memoのdataオプション、propsの設定

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Memo pageTitle="memo-pad" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Memo from './components/Memo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Memo
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Memo.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{ pageTitle}}</h1>
+  
+</template>
+
+<script>
+  export default {
+    title: "memoのタイトル",
+    text: "memoの本文",
+    props: {
+      pageTitle: String
+    }
+  }
+
+</script>
+
+<style scoped>
+
+
+</style>
